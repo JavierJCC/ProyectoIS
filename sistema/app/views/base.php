1955c4808b4e1ed0413a8071d0b69556b8b4c5dd
Ruben Murga <ruben.murga.d@gmail.com>
2016-10-05 17:09:12 -0500

    agregando herencia php

---
+++ sistema/app/views/base.php
@@ -0,0 +1,12 @@
+<?php require_once 'ti.php' ?>
+
+<html>
+  <body>
+  <h5>
+  HEY BASE
+  <?php emptyblock('main') ?>
+
+  </div>
+  </body>
+  </html>
+  
--- sistema/app/views/home/index.php
+++ sistema/app/views/home/index.php
@@ -1,2 +1,5 @@
-hola jajaja
-<?=$data['parametro']?>
+<?php include($_SERVER['DOCUMENT_ROOT']."/Proyecto_IS/ProyectoSemestreIS/sistema/app/views/base.php");?>
+
+ <?php startblock('main') ?>
+     The main content
+  <?php endblock() ?>
