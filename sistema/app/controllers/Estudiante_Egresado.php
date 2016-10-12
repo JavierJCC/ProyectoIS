b76757cac7c1d339b851b3e05eeeec5139d3733c
Ruben Murga <ruben.murga.d@gmail.com>
2016-10-11 20:50:51 -0500

    bd04

--- sistema/app/controllers/Estudiante_Egresado.php
+++ sistema/app/controllers/Estudiante_Egresado.php
@@ -19,6 +19,5 @@ class Estudiante_Egresado extends Controller
     $postdata = file_get_contents("php://input");
     $request = json_decode($postdata);
     $this->EE_modelo->insert_peticiones($request,$_SESSION["usuario"]->boleta);
-
   }
 }
