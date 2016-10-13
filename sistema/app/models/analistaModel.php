41784d21b43cdc0ca5647e5bde8198fb3553ecfc
Juan Guzman <g.juan.gc@gmail.com>
2016-10-12 23:37:30 -0500

    Update analistaModel.php

--- sistema/app/models/analistaModel.php
+++ sistema/app/models/analistaModel.php
@@ -7,8 +7,8 @@ class analistaModel
   private $db;
   private $connection;
   function __construct(){
-    $this->username="root";
-    $this->password="gcja100422";
+    $this->username="ingenieria2016";
+    $this->password="ingenieria";
     $this->db="ingenieria";
     $this->connection = new mysqli("127.0.0.1",$this->username,$this->password,$this->db);
     if(mysqli_connect_errno()){
