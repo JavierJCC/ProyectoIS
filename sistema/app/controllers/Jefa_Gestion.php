c3f4ad6d6e5be556966a1b3115464af9992b0123
JavierJCC <javisever2@gmail.com>
2016-10-19 22:56:28 -0500

    bitacora

--- sistema/app/controllers/Jefa_Gestion.php
+++ sistema/app/controllers/Jefa_Gestion.php
@@ -24,5 +24,14 @@ class Jefa_Gestion extends Controller{
     public function desactivar_cuentas(){
         $this->view('');
     }
-
+    
+    public function visualizar_bitacora(){
+    $EE_modelo = $this->model('jefagestionModel');
+    $motivos = $EE_modelo->bitacora();
+    $motivos2 = $EE_modelo->bitacora();
+    $this->view('jefaGestion/visualizar_bitacora', ['motivos'=> $motivos, 'motivos2'=> $motivos2]);
+    }
+    
+    
 }
+
